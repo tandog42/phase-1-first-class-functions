cd2705312c86ca2c3c0224ea1ec5ef245cfaeff8
Tanner <troxs690@gmail.com>
2022-01-14 09:57:37 -0600

    Done

---
+++ index.js
@@ -0,0 +1,29 @@
+
+
+
+function receivesAFunction(callBack) {
+    callBack()
+     }
+
+function returnsANamedFunction(){
+    return function name(){
+        console.log ('named function'); 
+    }
+}
+
+function returnsAnAnonymousFunction(){ 
+     return function() {
+
+}
+
+}
+
+
+
+
+
+
+
+
+
+
